9c5ed5aaaa3d31c39cf83479cbce45f9f2495c68
saidamst2003 <saida2003mst@gmail.com>
2024-12-10 16:30:45 +0100

    creation de menu

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        while(true){
+        System.out.println("-------------Menu------------\n");
+        System.out.println("1- Addition (+)\n");
+        System.out.println("2- Soustraction (-)\n");
+        System.out.println("3- Multiplication (*)\n");
+        System.out.println("4- Division(/)\n");
+        System.out.println("5- Puissance\n");
+        System.out.println("6- Racine carrée\n");
+        System.out.println("7- Factorielle\n");
+
