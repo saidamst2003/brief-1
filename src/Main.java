c36bfd5b61d86bcada60f952100da5b3f322f9c9
saidamst2003 <saida2003mst@gmail.com>
2024-12-11 16:27:11 +0100

    Ajouter l'opérations de calcul:factorialle

--- src/Main.java
+++ src/Main.java
@@ -1,79 +1,88 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        while (true) {
-            System.out.println("-------------Menu------------\n");
-            System.out.println("1- Addition (+)\n");
-            System.out.println("2- Soustraction (-)\n");
-            System.out.println("3- Multiplication (*)\n");
-            System.out.println("4- Division(/)\n");
-            System.out.println("5- Puissance\n");
-            System.out.println("6- Racine carrée\n");
-            System.out.println("7- Factorielle\n");
+        while(true){
+        System.out.println("-------------Menu------------\n");
+        System.out.println("1- Addition (+)\n");
+        System.out.println("2- Soustraction (-)\n");
+        System.out.println("3- Multiplication (*)\n");
+        System.out.println("4- Division(/)\n");
+        System.out.println("5- Puissance\n");
+        System.out.println("6- Racine carrée\n");
+        System.out.println("7- Factorielle\n");
 
-            System.out.println("choisir votre choix:");
-            Scanner sc = new Scanner(System.in);
-            int s = sc.nextInt();
-            System.out.println("donner premier nombre:");
-            int a = sc.nextInt();
-            System.out.println("donner deuxiem nombre:");
-            int b = sc.nextInt();
-            switch (s) {
-                case 1:
-                    Addition(a, b);
-                    break;
-                case 2:
-                    Soustraction(a, b);
-                    break;
-                case 3:
-                    Multiplication(a, b);
-                    break;
-                case 4:
-                    Division(a, b);
-                    break;
-                case 5:
-                    Puissance(a, b);
-                    break;
-                case 6:
-                    RacineCarrée(a);
-                    break;
-//                case 7:
-//                    Factorielle(a);
-//                    break;
-                case 8:
-                    return;
-            }
-
+        System.out.println("choisir votre choix:");
+        Scanner sc= new Scanner(System.in);
+        int s= sc.nextInt();
+        System.out.println("donner premier nombre:");
+        int a= sc.nextInt();
+        System.out.println("donner deuxiem nombre:");
+        int b= sc.nextInt();
+        switch (s){
+            case 1:
+                Addition(a,b);
+                break;
+            case 2:
+                Soustraction(a,b);
+                break;
+            case 3:
+                Multiplication(a,b);
+                break;
+            case 4:
+                Division(a,b);
+                break;
+            case 5:
+               Puissance(a,b);
+                break;
+            case 6:
+                RacineCarrée(a);
+                break;
+            case 7:
+                Factorielle(a);
+                break;
+            case 8:
+                return ;
         }
-    }
-
-    public static void Addition(double a, double b) {
-        System.out.println("addition : " + (a + b));
 
     }
-
-    public static void Soustraction(double a, double b) {
-        System.out.println("soustraction : " + (a - b));
-
     }
 
-    public static void Multiplication(double a, double b) {
-        System.out.println("Multiplication  : " + (a * b));
+    public static void Addition(double a,double b){
+        System.out.println("addition : " + (a+b));
 
     }
-
-    public static void Division(int a, int b) {
-        System.out.println("Division  : " + (a / b));
-
-    }
-
-    public static void Puissance(double a, double b) {
-        System.out.println("Puissance : " + Math.pow(a, b));
+    public static void Soustraction(double a,double b){
+        System.out.println("soustraction : " + (a-b));
 
     }
+    public static void Multiplication(double a,double b){
+        System.out.println("Multiplication  : " + (a*b));
 
-    public static void RacineCarrée(double a) {
+    }
+    public static void Division(int a,int b){
+        System.out.println("Division  : " + (a/b));
+
+    }
+    public static void Puissance(double a, double b){
+        System.out.println("Puissance : " + Math.pow(a,b));
+
+    }
+    public static void RacineCarrée(double a){
         System.out.println("RacineCarrée : " + Math.sqrt(a));
+
+    }
+    public static void Factorielle(double a){
+        int fact=1;
+        if (a==1||a==0) {
+            fact =1;
+        }else{
+            for (int i=2;i<=a;i++){
+                fact = fact * i;
+            }
+        }
+
+        System.out.println(" Factorielle  : " + fact );
+
 
     }
 }
