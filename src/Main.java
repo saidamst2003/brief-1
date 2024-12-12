d3e29b52a9e901e30ba6bf4b620d2d4eb876c4e3
saidamst2003 <saida2003mst@gmail.com>
2024-12-12 15:45:44 +0100

    modification

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         while(true){
@@ -14,31 +15,57 @@ public class Main {
         System.out.println("choisir votre choix:");
         Scanner sc= new Scanner(System.in);
         int s= sc.nextInt();
-        System.out.println("donner premier nombre:");
-        int a= sc.nextInt();
-        System.out.println("donner deuxiem nombre:");
-        int b= sc.nextInt();
+        //System.out.println("donner premier nombre:");
+       // int a= sc.nextInt();
+       // System.out.println("donner deuxiem nombre:");
+        //int b= sc.nextInt();
         switch (s){
             case 1:
+
+                System.out.println("donner premier nombre:");
+                int a= sc.nextInt();
+                System.out.println("donner deuxiem nombre:");
+                int b= sc.nextInt();
                 Addition(a,b);
+
                 break;
             case 2:
-                Soustraction(a,b);
+                System.out.println("donner premier nombre:");
+                int s1= sc.nextInt();
+                System.out.println("donner deuxiem nombre:");
+                int s2= sc.nextInt();
+                Soustraction(s1, s2);
                 break;
             case 3:
-                Multiplication(a,b);
+                System.out.println("donner premier nombre:");
+                int m1= sc.nextInt();
+                System.out.println("donner deuxiem nombre:");
+                int m2= sc.nextInt();
+                Multiplication(m1,m2);
                 break;
             case 4:
-                Division(a,b);
+                System.out.println("donner premier nombre:");
+                int v1= sc.nextInt();
+                System.out.println("donner deuxiem nombre:");
+                int v2= sc.nextInt();
+                Division(v1,v2);
                 break;
             case 5:
-               Puissance(a,b);
+                System.out.println("donner premier nombre:");
+                int l1= sc.nextInt();
+                System.out.println("donner deuxiem nombre:");
+                int l2= sc.nextInt();
+               Puissance(l1,l2);
                 break;
             case 6:
-                RacineCarrée(a);
+                System.out.println("donner un nombre:");
+                int u= sc.nextInt();
+                RacineCarrée(u);
                 break;
             case 7:
-                Factorielle(a);
+                System.out.println("donner un nombre:");
+                int z= sc.nextInt();
+                Factorielle(z);
                 break;
             case 8:
                 return ;
@@ -59,17 +86,33 @@ public class Main {
         System.out.println("Multiplication  : " + (a*b));
 
     }
-    public static void Division(int a,int b){
+   /* public static void Division(int a,int b){
+        while (b==0){
+            System.out.println(" error : entrer un nombre different de 0!! ");
+            Scanner sc = new Scanner(System.in);
+          b = sc.nextInt();
+        }
         System.out.println("Division  : " + (a/b));
 
-    }
+    }*/
+   public static void Division(double a, double b) {
+       if (b==0){
+           System.out.println("error : la division par 0 est impossible!!");
+       }else {
+           System.out.println("Division  : " + (a/b));
+       }
+
+   }
     public static void Puissance(double a, double b){
         System.out.println("Puissance : " + Math.pow(a,b));
 
     }
     public static void RacineCarrée(double a){
-        System.out.println("RacineCarrée : " + Math.sqrt(a));
-
+     if(a<0){
+         System.out.println("error: impossible de calculer un racine d/'un nombre negatif!!");
+     }else {
+         System.out.println("RacineCarrée : " + Math.sqrt(a));
+     }
     }
     public static void Factorielle(double a){
         int fact=1;
